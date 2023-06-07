73375d2ea575c4be0b2aa7bcaf6f34a8290599a6
Martin <mar_1905@hotmail.es>
2023-06-07 10:47:38 -0500

    Cambios

--- app/components/Comentarios.tsx
+++ app/components/Comentarios.tsx
@@ -7,7 +7,7 @@ const Slider = dynamic(() => import('react-slick'))
 export const Comentarios = () => {
     const [settings] = useSlider({ valor: 3 })
     return (
-        <section className=" py-20">
+        <section className="bg-comentarios py-20">
             <article className="container mx-auto">
                 <h4 className="font-bold text-white text-center text-3xl">Opiniones de nuestros alumnos</h4>
                 <p className="text-center text-white mt-5 mb-10">Alumnos satisfecho</p>
