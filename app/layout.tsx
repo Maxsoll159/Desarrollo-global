5f24f526bb093fc2d94b21bab2c4f69757e8991b
Martin <mar_1905@hotmail.es>
2023-06-07 14:34:37 -0500

    probando

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import '@flaticon/flaticon-uicons/css/regular/all.css'
+
 import { Navbar } from '@/components/Navbar/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
