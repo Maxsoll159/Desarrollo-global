d74cf28d30286aa45a0636645c8d2ad24a47b1f2
Martin Rios Tineo <68676050+Maxsoll159@users.noreply.github.com>
2023-06-12 16:01:50 -0500

    Probando

--- app/components/Opciones.tsx
+++ app/components/Opciones.tsx
@@ -1,54 +1,12 @@
 import Link from "next/link"
 
-import { RiBookOpenLine, RiAwardLine } from "react-icons/ri";
-import { FiFileText, FiHome, FiVideo } from "react-icons/fi"
-import { AiOutlinePlusCircle } from 'react-icons/ai'
 export const Opciones = () => {
     return (
         <section className="">
             <article className="px-5 py-10 md:py-20 container mx-auto">
                 <h3 className="text-3xl text-center text-[#374957] font-bold">Hola, ¿Qué estas buscando aprender hoy?</h3>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-14 gap-2">
-                    <Link href="/sd" className="group border-2 rounded-md p-5 md:p-10 flex flex-col justify-center items-center mx-auto hover:bg-[#2a50e4] w-full md:w-[240px]">
-                        <RiBookOpenLine className="text-[60px] font-dark text-[#2a50e4] group-hover:text-white" />
-                        <p className="text-[#7a8897] text-2xl font-bold group-hover:text-white">Cursos</p>
-                        <div className="items-center gap-2 px-3 py-1 rounded-md mt-2 flex border bg-blue-desarrollo text-white">
-                            <p className="font-semibold">Ver mas</p>
-                            <AiOutlinePlusCircle className="font-bold text-xl" />
-                        </div>
-                    </Link>
-                    <Link href="/sd" className="group border-2 rounded-md p-5 md:p-10 flex flex-col justify-center items-center mx-auto hover:bg-[#2a50e4] w-full md:w-[240px] ">
-                        <RiAwardLine className="text-[60px] font-dark text-[#2a50e4] group-hover:text-white" />
-                        <p className="text-[#7a8897] text-2xl font-bold group-hover:text-white">Diplomas</p>
-                        <div className="items-center gap-2 px-3 py-1 rounded-md mt-2 flex border bg-blue-desarrollo text-white">
-                            <p className="font-semibold">Ver mas</p>
-                            <AiOutlinePlusCircle className="font-bold text-xl" />
-                        </div>
-                    </Link>
-                    <Link href="/sd" className="group border-2 rounded-md p-5 md:p-10 flex flex-col justify-center items-center mx-auto hover:bg-[#2a50e4] w-full md:w-[240px] ">
-                        <FiFileText className="text-[60px] font-dark text-[#2a50e4] group-hover:text-white" />
-                        <p className="text-[#7a8897] text-2xl font-bold group-hover:text-white">Diplomados</p>
-                        <div className="items-center gap-2 px-3 py-1 rounded-md mt-2 flex border bg-blue-desarrollo text-white">
-                            <p className="font-semibold">Ver mas</p>
-                            <AiOutlinePlusCircle className="font-bold text-xl" />
-                        </div>
-                    </Link>
-                    <Link href="/sd" className="group border-2 rounded-md p-5 md:p-10 flex flex-col justify-center items-center mx-auto hover:bg-[#2a50e4] w-full md:w-[240px] ">
-                        <FiHome className="text-[60px] font-dark text-[#2a50e4] group-hover:text-white" />
-                        <p className="text-[#7a8897] text-2xl font-bold group-hover:text-white">In House</p>
-                        <div className="items-center gap-2 px-3 py-1 rounded-md mt-2 flex border bg-blue-desarrollo text-white">
-                            <p className="font-semibold">Ver mas</p>
-                            <AiOutlinePlusCircle className="font-bold text-xl" />
-                        </div>
-                    </Link>
-                    <Link href="/sd" className="group border-2 rounded-md p-5 md:p-10 flex flex-col justify-center items-center mx-auto hover:bg-[#2a50e4] w-full md:w-[240px] ">
-                        <FiVideo className="text-[60px] font-dark text-[#2a50e4] group-hover:text-white" />
-                        <p className="text-[#7a8897] text-2xl font-bold group-hover:text-white">Grabados</p>
-                        <div className="items-center gap-2 px-3 py-1 rounded-md mt-2 flex border bg-blue-desarrollo text-white">
-                            <p className="font-semibold">Ver mas</p>
-                            <AiOutlinePlusCircle className="font-bold text-xl" />
-                        </div>
-                    </Link>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-14 gap-2 bg-gray-200 rounded-full">
+                    <Link href="//">sd </Link>
                 </div>
             </article>
         </section>
--- components/Slider/Benefits.tsx
+++ components/Slider/Benefits.tsx
@@ -6,34 +6,31 @@ export const Benefits = () => {
             <article className="container mx-auto">
                 <div className="grid grid-cols-2 lg:grid-cols-4 p-5">
                 <div className="flex items-center gap-3 mx-auto">
-
-
-
-                        <TbFileCertificate className="text-[#485BFF] text-6xl" />
+                        <Image src="/img/IconBeneficio1.webp" alt="Desarrollo Global - Certificacion Universitaria" width={100} height={104} className="w-[50px] md:w-auto" />
                         <div>
                             <span className="text-base md:text-lg">Certificado de Calidad</span>
-                            <p className="text-sm md:text-3xl font-bold text-[#485BFF]">ISO 9001-2015</p>
+                            <p className="text-sm md:text-3xl font-bold text-blue-desarrollo">ISO 9001-2015</p>
                         </div>
                     </div>
                     <div className="flex items-center gap-3 mx-auto">
-                        <Image src="/img/beneficios2.webp" alt="Desarrollo Global - Certificacion Universitaria" width={100} height={104} className="w-[50px] md:w-auto" />
+                        <Image src="/img/IconBeneficio2.webp" alt="Desarrollo Global - Certificacion Universitaria" width={100} height={104} className="w-[50px] md:w-auto" />
                         <div>
                             <span className="text-base md:text-lg">Respaldo</span>
-                            <p className="text-sm md:text-3xl font-bold text-[#485BFF]">Universitario</p>
+                            <p className="text-sm md:text-3xl font-bold text-blue-desarrollo">Universitario</p>
                         </div>
                     </div>
                     <div className="flex items-center gap-3 mx-auto">
-                        <Image src="/img/beneficios3.webp" alt="Desarrollo Global - Mas de 50000 alumnos capacitados" width={100} height={104} className="w-[50px] md:w-auto" />
+                        <Image src="/img/IconBeneficio3.webp" alt="Desarrollo Global - Mas de 50000 alumnos capacitados" width={100} height={104} className="w-[50px] md:w-auto" />
                         <div>
                             <span className="text-base md:text-lg">Alumnos Capacitados</span>
-                            <p className="text-sm md:text-3xl font-bold text-[#485BFF]">+ 50,000</p>
+                            <p className="text-sm md:text-3xl font-bold text-blue-desarrollo">+ 50,000</p>
                         </div>
                     </div>
                     <div className="flex items-center gap-3 mx-auto">
-                        <Image src="/img/beneficios4.webp" alt="Desarrollo Global - Mas de 12 años capacitando" width={100} height={104} className="w-[50px] md:w-auto" />
+                        <Image src="/img/IconBeneficio4.webp" alt="Desarrollo Global - Mas de 12 años capacitando" width={100} height={104} className="w-[50px] md:w-auto" />
                         <div>
                             <span className="text-base md:text-lg">Capacitando</span>
-                            <p className="text-sm md:text-3xl font-bold text-[#485BFF]">+ 12 años</p>
+                            <p className="text-sm md:text-3xl font-bold text-blue-desarrollo">+ 12 años</p>
                         </div>
                     </div>
                 </div>
