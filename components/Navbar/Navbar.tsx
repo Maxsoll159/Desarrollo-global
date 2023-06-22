cafce3d5f74b26f8654411790e519cd5656e21f5
Martin Rios Tineo <68676050+Maxsoll159@users.noreply.github.com>
2023-06-22 12:01:35 -0500

    Menu Logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,24 +10,24 @@ export const Navbar = () => {
             <nav className="px-0 lg:px-5">
                 <div className="py-2">
                     <div className="flex justify-between w-full items-center">
-                        <Image src="/img/DG-Logotipo.webp" alt="Desarrollo Global" width={226} height={56} />
+                        <Image src="/img/LogoDesarrolloGlobalCo.webp" alt="Desarrollo Global" width={226} height={56} />
 
-                        <button className="p-2 lg:hidden" onClick={() => setMostrar(!mostrar)}>
-                            <div className="bg-white h-0.5 w-6"></div>
-                            <div className="bg-white h-0.5 w-6 mt-1"></div>
-                            <div className="bg-white h-0.5 w-6 mt-1"></div>
+                        <button className="p-2 lg:hidden" onClick={() => setMostrar(!mostrar)} name="Buttom Hamburguesa">
+                            <div className="bg-black h-0.5 w-6"></div>
+                            <div className="bg-black h-0.5 w-6 mt-1"></div>
+                            <div className="bg-black h-0.5 w-6 mt-1"></div>
                         </button>
 
 
                         <div className={`items-center gap-7 flex lg:justify-end flex-col lg:flex-row absolute lg:static top-full transition-all lg:!translate-x-0 lg:bg-transparent lg:pt-0 pt-5 bg-white w-full -translate-x-full ${mostrar && "translate-x-0"}`}>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 ease-in-out hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Inicio</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Cursos</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Diplomas</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Diplomados</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">InHouse</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Seminarios</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Contacto</Link>
-                            <Link href="/" className="text-link font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Blog</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 ease-in-out hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Inicio</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Cursos</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Diplomas</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Diplomados</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">InHouse</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Seminarios</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Contacto</Link>
+                            <Link href="/" className="font-semibold transition-all duration-300 hover:text-blue-desarrollo border-b-[#485bff] hover:border-b-white">Blog</Link>
                         </div>
                     </div>
                 </div>
